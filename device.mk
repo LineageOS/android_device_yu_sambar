08e0223c0ebe2cb4379d47f30d502fd5abca02f1
Scott Mertz <scott@cyngn.com>
2015-07-01 01:29:10 -0700

    sambar: add keyhandler
    - Imported from CyanogenMod/android_device_oppo_common
    
    Change-Id: I7ad05ce67438b7e6ef8d04d9f0ba010ab66db47e

--- device.mk
+++ device.mk
@@ -162,6 +162,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.keyhandler
+
+PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+
+# never dexopt the keyhandler
+$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
