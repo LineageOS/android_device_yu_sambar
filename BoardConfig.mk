59c641ecc897f762051430c4e21c2f32f674694e
Steve Kondik <steve@cyngn.com>
2015-07-09 21:31:06 -0700

    sambar: Include kernel debug features on non-user builds
    Change-Id: I0347cd5c7b67a91a5bc961a265c0e2fd09de2f55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,11 @@ endif
 TARGET_KERNEL_CONFIG := cyanogenmod_sambar_defconfig
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8994
 
+# Enable DIAG on debug builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
+endif
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
 
