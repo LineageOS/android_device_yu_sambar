1f24b67fd24a3da550bb47f483e0174011ff1e83
Steve Kondik <steve@cyngn.com>
2015-11-03 11:15:22 -0800

    sambar: Get rid of custom mkbootimg
     * We have generic support for this finally.
    
    Change-Id: I82facea2f29fc2f921bd5179f32f5b8ae82f9648

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ endif
 TARGET_CPU_CORTEX_A53 := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_PAGESIZE := 4096
