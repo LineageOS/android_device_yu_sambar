d2a615cd8583c4e64f484e8fb69eed4c561351eb
Scott Mertz <scott@cyngn.com>
2015-11-10 17:30:03 -0800

    sambar: audio: upstream M changes
    CAF added the low-latency by default feature, and renamed
    it. Update our reference to match CAF implementation.
    
    Switch to CAF audio policy implementation and configuration.
    
    Rename effects to match upstream libraries.
    
    SAMBAR-1044
    Change-Id: I8e7efd76bef10511c665ad05666b5c19eca9d4c2

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/Speaker_cal.acdb:system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/srs_processing.cfg:system/etc/srs/srs_processing.cfg \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
