1c87492b3115e654c65dfd16646ea6631d137985
HridayHS <hridaysharma42@gmail.com>
2016-01-06 13:43:01 +0530

    sambar: audio_amplifier: remove unwanted space
    Change-Id: I29c9ca00d0cdd13fae4faefeb28d8c4dbb7246bf
    Signed-off-by: HridayHS <hridaysharma42@gmail.com>

--- audio_amplifier/audio_amplifier.c
+++ audio_amplifier/audio_amplifier.c
@@ -1,7 +1,5 @@
-
-
 /*
- * Copyright (C) 2015 The CyanogenMod Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
